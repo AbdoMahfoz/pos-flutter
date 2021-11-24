2d7cd155467fd67c2897ee6c9f7f887383712d64
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2021-11-24 16:21:08 +0200

    WebMainScreen enhancements

--- lib/common/LabeledCheckbox.dart
+++ lib/common/LabeledCheckbox.dart
@@ -7,6 +7,7 @@ class LabeledCheckbox extends StatelessWidget {
   final bool enabled;
   final Color enabledColor;
   final Color disabledColor;
+  final TextDirection direction;
 
   const LabeledCheckbox(
       {Key? key,
@@ -15,41 +16,48 @@ class LabeledCheckbox extends StatelessWidget {
       this.label,
       this.enabledColor = Colors.white,
       this.disabledColor = Colors.white38,
+      this.direction = TextDirection.rtl,
       required this.onChange})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dir = this.direction == TextDirection.rtl
+        ? TextDirection.ltr
+        : TextDirection.rtl;
     var color = MaterialStateColor.resolveWith(this.enabled
         ? (states) => this.enabledColor
         : (states) => this.disabledColor);
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Wrap(
-          alignment: WrapAlignment.end,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          spacing: 0,
-          children: [
-            if (this.label != null)
-              Transform.translate(
-                  offset: const Offset(15, 0),
-                  child: Text(
-                    this.label!,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  )),
-            AbsorbPointer(
-              absorbing: !this.enabled,
-              child: Checkbox(
-                value: this.value,
-                onChanged: this.onChange,
-                fillColor: color,
-                checkColor:
-                    MaterialStateColor.resolveWith((states) => Colors.black),
-                activeColor: color,
+    return Directionality(
+      textDirection: dir,
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Wrap(
+            alignment: WrapAlignment.end,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            spacing: 0,
+            children: [
+              if (this.label != null)
+                Transform.translate(
+                    offset: const Offset(15, 0),
+                    child: Text(
+                      this.label!,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    )),
+              AbsorbPointer(
+                absorbing: !this.enabled,
+                child: Checkbox(
+                  value: this.value,
+                  onChanged: this.onChange,
+                  fillColor: color,
+                  checkColor:
+                      MaterialStateColor.resolveWith((states) => Colors.black),
+                  activeColor: color,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/web/main/webMainScreen.dart
+++ lib/screens/web/main/webMainScreen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:posapp/common/BaseWidgets.dart';
 import 'package:posapp/common/FilledTextField.dart';
+import 'package:posapp/common/LabeledCheckbox.dart';
+import 'package:posapp/common/PrimaryButton.dart';
+import 'package:posapp/common/PrimaryTextField.dart';
 import 'package:posapp/viewmodels/web/webMainViewModel.dart';
 
 class WebMainScreen extends ScreenWidget {
@@ -26,91 +29,180 @@ class WebMainScreenState
         .bodyText2!
         .copyWith(fontWeight: FontWeight.bold);
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Flex(
-        direction: Axis.vertical,
-        children: [
-          Container(
-            width: double.infinity,
-            color: Colors.grey[800],
-            child: Column(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Stack(children: [
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(
+                Color.fromRGBO(0, 0, 0, 0.7), BlendMode.darken),
+            child: Flex(
+              direction: Axis.vertical,
               children: [
-                SizedBox(height: 30),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                    child: Text("All Items",
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold)),
+                Container(
+                  width: double.infinity,
+                  color: Colors.grey[800],
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                          child: Text("All Items",
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50.0, vertical: 20.0),
+                        child: FilledTextField(
+                          onTextChanged: (newVal) {},
+                          fontSize: 30,
+                          hintText: "Search",
+                          fillColor: Colors.grey[700]!,
+                          hoverColor: Colors.grey[600]!,
+                          focusColor: Colors.grey[600]!,
+                          textColor: Colors.white,
+                          borderRadius: 35,
+                          innerPadding: EdgeInsets.symmetric(
+                              vertical: 18, horizontal: 20),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 50.0, vertical: 20.0),
-                  child: FilledTextField(
-                    onTextChanged: (newVal) {},
-                    fontSize: 30,
-                    hintText: "Search",
-                    fillColor: Colors.grey[700]!,
-                    hoverColor: Colors.grey[600]!,
-                    focusColor: Colors.grey[600]!,
-                    textColor: Colors.white,
-                    borderRadius: 35,
-                    innerPadding:
-                        EdgeInsets.symmetric(vertical: 18, horizontal: 20),
-                  ),
+                Flexible(
+                  child: DataTable2(
+                      showCheckboxColumn: true,
+                      decoration: BoxDecoration(
+                          color: Colors.black54,
+                          border: Border.symmetric(
+                              horizontal: BorderSide(
+                                  color: Theme.of(context).backgroundColor,
+                                  width: 1))),
+                      border: TableBorder.symmetric(
+                          inside: BorderSide(color: Colors.grey, width: 1)),
+                      checkboxHorizontalMargin: 10,
+                      scrollController: this.scrollController,
+                      columns: [
+                        DataColumn2(
+                            size: ColumnSize.L,
+                            label: Text(
+                              "Name",
+                              style: headerStyle,
+                            )),
+                        DataColumn2(
+                            size: ColumnSize.S,
+                            label: Text("Quantity", style: headerStyle),
+                            numeric: true),
+                        DataColumn2(
+                            size: ColumnSize.M,
+                            label: Text("Category", style: headerStyle)),
+                        DataColumn2(
+                            size: ColumnSize.M,
+                            label: Text("Car model", style: headerStyle)),
+                        DataColumn2(
+                            size: ColumnSize.S,
+                            label: Text("Condition", style: headerStyle)),
+                        DataColumn2(
+                            size: ColumnSize.S,
+                            label: Text(
+                              "Actions",
+                              style: headerStyle,
+                            ))
+                      ],
+                      rows: List<DataRow2>.generate(
+                          20,
+                          (index) => DataRow2(cells: [
+                                DataCell(Text("Item name")),
+                                DataCell(Text("500")),
+                                DataCell(Text("Category name")),
+                                DataCell(Text("Car model name")),
+                                DataCell(Text("Is new",
+                                    style: TextStyle(color: Colors.green))),
+                                DataCell(Row(
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(Icons.edit),
+                                        onPressed: () {},
+                                        color: Colors.white),
+                                    SizedBox(width: 10),
+                                    IconButton(
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {},
+                                        color: Colors.white)
+                                  ],
+                                ))
+                              ]))),
                 )
               ],
             ),
           ),
-          Flexible(
-            child: DataTable2(
-                showCheckboxColumn: true,
-                decoration: BoxDecoration(
-                    color: Colors.black54,
-                    border: Border.symmetric(
-                        horizontal: BorderSide(
-                            color: Theme.of(context).backgroundColor,
-                            width: 1))),
-                border: TableBorder.symmetric(
-                    inside: BorderSide(color: Colors.grey, width: 1)),
-                checkboxHorizontalMargin: 10,
-                scrollController: this.scrollController,
-                columns: [
-                  DataColumn2(
-                      size: ColumnSize.L,
-                      label: Text(
-                        "Name",
-                        style: headerStyle,
-                      )),
-                  DataColumn2(
-                      size: ColumnSize.S,
-                      label: Text("Quantity", style: headerStyle),
-                      numeric: true),
-                  DataColumn2(
-                      size: ColumnSize.M,
-                      label: Text("Category", style: headerStyle)),
-                  DataColumn2(
-                      size: ColumnSize.S,
-                      label: Text("Car model", style: headerStyle)),
-                  DataColumn2(
-                      size: ColumnSize.S,
-                      label: Text("Condition", style: headerStyle)),
-                ],
-                rows: List<DataRow2>.generate(
-                    20,
-                    (index) => DataRow2(cells: [
-                          DataCell(Text("Item name")),
-                          DataCell(Text("500")),
-                          DataCell(Text("Category name")),
-                          DataCell(Text("Car model name")),
-                          DataCell(Text("Is new",
-                              style: TextStyle(color: Colors.green)))
-                        ]))),
-          )
-        ],
-      ),
-    );
+          Center(
+            child: Container(
+              height: 500.0,
+              width: 800.0,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(36, 33, 33, 1.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: [
+                    BoxShadow(
+                        spreadRadius: 0.3,
+                        blurRadius: 5,
+                        color: Colors.black,
+                        offset: Offset(0, 5))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Text("Edit item",
+                        style: Theme.of(context).textTheme.headline1),
+                    SizedBox(height: 10),
+                    PrimaryTextField(
+                      label: "Name",
+                      onChanged: (newName) {},
+                      valueDirection: TextDirection.ltr,
+                      labelDirection: TextDirection.ltr,
+                    ),
+                    PrimaryTextField(
+                      label: "Quantity",
+                      onChanged: (newName) {},
+                      valueDirection: TextDirection.ltr,
+                      labelDirection: TextDirection.ltr,
+                    ),
+                    PrimaryTextField(
+                      label: "Category (Combobox)",
+                      onChanged: (newName) {},
+                      valueDirection: TextDirection.ltr,
+                      labelDirection: TextDirection.ltr,
+                    ),
+                    PrimaryTextField(
+                      label: "CarModel (Combobox)",
+                      onChanged: (newName) {},
+                      valueDirection: TextDirection.ltr,
+                      labelDirection: TextDirection.ltr,
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(4.0, 0, 0, 0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: LabeledCheckbox(
+                          value: true,
+                          onChange: (newVal) {},
+                          label: "Is new",
+                          direction: TextDirection.ltr,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    PrimaryButton(onPressed: () {}, text: "Save")
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]));
   }
 }
